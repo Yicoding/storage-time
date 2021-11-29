a8f06fe4aa9dfbb9625ba50f3d813ec6be2d7177
Yicoding <804413572@qq.com>
2021-11-29 17:27:28 +0800

    升级

--- example/README.md
+++
@@ -1,2 +0,0 @@
-# webpack-demo
-babel篇-打包库文件
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        }
+      }
+    ]
+  },
+}
